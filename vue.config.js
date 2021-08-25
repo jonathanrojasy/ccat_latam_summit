5c24a7080d531bc30665b27c1b2249bb42075b08
jonathanrojasy <jonathanry98@gmail.com>
2021-08-24 23:17:38 -0500

    update

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    pages: {
-        index: {
-            // entry for the page
-            entry: 'src/main.js',
-            title: 'CCAT Latam Summit',
-        },
-    }
-}
